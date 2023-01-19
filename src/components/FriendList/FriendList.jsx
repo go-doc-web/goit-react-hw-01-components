62e1a1f6d6ca3ed413b85c0ecc723c75fbcb9330
Oleg Gubskiy <gubskyolegod@gmail.com>
2023-01-19 12:33:11 +0200

    Work under Error

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -3,38 +3,26 @@ import css from './friendList.module.css';
 
 import FriendListItem from './FriendListItem/FriendListItem';
 
-const FriendList =({items})=>{
-  
-   const elements = items.map(({id,...itemProps})=><FriendListItem key={id} {...itemProps}/>)
-return (
-   
-      <ul className={css.list}>
-         {elements}
-      </ul>
-   
-)
-}
+const FriendList = ({ items }) => {
+  const elements = items.map(({ id, ...itemProps }) => (
+    <FriendListItem key={id} {...itemProps} />
+  ));
+  return <ul className={css.list}>{elements}</ul>;
+};
 
 export default FriendList;
 
 FriendList.defaultProps = {
-   items: [],
-
+  items: [],
 };
- 
-FriendList.propTypes = {
-   items: PropTypes.arrayOf(
-      PropTypes.shape({
-         avatar:PropTypes.string,
-            id:PropTypes.number.isRequired,
-            isOnline:PropTypes.bool.isRequired,
-            name:PropTypes.string.isRequired
-      })
-           
-   )
-}
-   
-// List.propTypes = { title: PropTypes.string, items: PropTypes.arrayOf(
-// PropTypes.shape({ id: PropTypes.string.isRequired, title:
-// PropTypes.string.isRequired, year: PropTypes.string.isRequired, }) ), };
 
+FriendList.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string,
+      id: PropTypes.number.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ),
+};
--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -1,42 +1,30 @@
 import PropTypes from 'prop-types';
 import css from './friendListItem.module.css';
 
+const FriendListItem = ({ name, avatar, isOnline }) => {
+  let fullClassNameStatus = isOnline
+    ? `${css.status} `
+    : `${css.status} ${css.active} `;
 
-const FriendListItem =({name, avatar,isOnline})=>{
-   let  fullClassNameStatus = isOnline ? `${css.status} `:`${css.status} ${css.active} `;
-   
-  
-   return (
-      
-      <li className={css.item}>
-         <span className={fullClassNameStatus }></span>
-         <div className={css.thumb}>
-         <img className={avatar} src={avatar} alt="User avatar" width="120" />
-         </div>
-         <p className={css.name}>{name}</p>
-      </li>
-   )
-}
-
+  return (
+    <li className={css.item}>
+      <span className={fullClassNameStatus}></span>
+      <div className={css.thumb}>
+        <img className={avatar} src={avatar} alt="User avatar" width="120" />
+      </div>
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+};
 
 export default FriendListItem;
 
 FriendListItem.defaultProps = {
-   avatar:'https://cdn-icons-png.flaticon.com/512/1077/1077012.png'
- }
+  avatar: 'https://cdn-icons-png.flaticon.com/512/1077/1077012.png',
+};
 
-
-
-FriendListItem.propTypes ={
-   items:PropTypes.arrayOf({
-      item:PropTypes.shape({
-         avatar:PropTypes.string,
-        
-         isOnline:PropTypes.bool.isRequired,
-         name:PropTypes.string.isRequired
-   
-   
-      })
-   })
-   }
-
+FriendListItem.propTypes = {
+  avatar: PropTypes.string,
+  isOnline: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
+};
